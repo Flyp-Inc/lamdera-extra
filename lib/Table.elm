f9e5dfe66609e173781d8ce90b89838bcb30ea92
John Pavlick <jmpavlick@gmail.com>
2024-07-29 06:53:53 -0400

    master: savepoint

--- lib/Table.elm
+++ lib/Table.elm
@@ -1,5 +1,7 @@
 module Table exposing
     ( Table
+    , Config, define
+    , Index, withIndex, toIndex
     , init, cons, update, delete
     , getById
     )
@@ -15,6 +17,13 @@ the `Table` type takes responsibility for mediating those operations.
 @docs Table
 
 
+# Configuration
+
+@docs Config, define
+
+@docs Index, withIndex, toIndex
+
+
 # Commands
 
 @docs init, cons, update, delete
@@ -28,6 +37,7 @@ the `Table` type takes responsibility for mediating those operations.
 
 import Dict
 import L
+import Murmur3
 import Random
 import Task
 import Time
@@ -38,6 +48,152 @@ import UUID
 -}
 type Table a
     = Table (Dict.Dict String { value : a, createdAt : Time.Posix }) Random.Seed (Dict.Dict String { value : a, deletedAt : Time.Posix })
+
+
+type Table_ a
+    = Table_
+        { rows : Dict.Dict String { value : a, createdAt : Time.Posix }
+        , seed : Random.Seed
+        , deleted : Dict.Dict String { value : a, deletedAt : Time.Posix }
+        , index : Dict.Dict Int (List String)
+        }
+
+
+{-| Represent configuration details for a table.
+
+Internally, configuration is:
+
+    - A mapping between a `String` and the ID type for your table
+    - A representation of any indexes
+
+-}
+type Config id a
+    = Config (String -> id) (List (Index a))
+
+
+{-| Define a table's configuration.
+
+The minimum-required configuration for a table is "a mapping between a `String` and an ID type". ID values are represented interally as a `String`, so
+the path-of-least-resistance here is to just use the `identity` function; but for a tiny bit more effort, you can have an `Id` type that is unique to your table -
+and that makes your code quite a bit easier to read, and it gives the compiler more information so that it can keep you from accidentally using the `String`-value of
+a row's ID for the wrong thing!
+
+Here's what this could look like:
+
+    ```
+    module User exposing (..)
+
+    import Table
+
+    type alias Record =
+        { emailAddress : String }
+
+    type Id
+        = Id String
+
+    config : Table.Config Id Record
+    config =
+        Table.define Id
+    ```
+
+-}
+define : (String -> id) -> Config id a
+define toId =
+    Config toId []
+
+
+{-| Represent an index for a table.
+
+Internally, an index is:
+
+    - A unique identifier (i.e., the index name)
+    - A map between a property on an `a` and an `Int`
+
+When you _query_ a table, you query it by its indexes. This doesn't mean that you need to create an index for every column! The columns
+that you should create indexes for are the columns that will help you identify a record, or identify a group of records by some column that
+is a member of a smaller set compared to other columns.
+
+Consider the following:
+
+    ```
+    module User exposing (..)
+
+    import Table
+
+    type alias Record =
+        { emailAddress : String
+        , catchphrase : String
+        , role : Role
+        }
+
+    type Role
+        = Admin
+        | Member
+    ```
+
+If we need to run queries against `User.Record` to find all users that are `Admin` users, it would make sense to create an index on the `.role`
+column, because the `Role` type is a member of a set that only has two values - `Admin` or `Member`. It wouldn't make much sense to create an index
+on the `.catchphrase` column - since that column is likely going to be unique for each user, anyway - so if we really needed to do a query against
+the `.catchphrase` column, we would have to consider every record separately, no matter what.
+
+-}
+type Index a
+    = Index (a -> Int)
+
+
+{-| Add an `Index a` to a `Table a`.
+-}
+withIndex : Index a -> Config id a -> Config id a
+withIndex idx (Config toId indexes) =
+    Config toId <| idx :: indexes
+
+
+{-| Create an `Index a` by providing a unique name for the column being indexed, and a string representation of that column's value.
+
+For example:
+
+    ```
+    module User exposing (..)
+
+    import Table
+
+
+    type alias Record =
+        { emailAddress : String
+        , catchphrase : String
+        , role : Role
+        }
+
+
+    type Role
+        = Admin
+        | Member
+
+
+    idxRole : Table.Index Record
+    idxRole =
+        Table.toIndex "Role"
+            (\role ->
+                case role of
+                    Admin ->
+                        "Admin"
+
+                    Member ->
+                        "Member"
+            )
+    ```
+
+This is handled as a separate operation from `withIndex`, since you will need to have an `Index a` value to use as a parameter for your query.
+
+-}
+toIndex : String -> (a -> String) -> Index a
+toIndex name accessor =
+    let
+        hashSeed : Int
+        hashSeed =
+            Murmur3.hashString 0 name
+    in
+    Index (\value -> Murmur3.hashString hashSeed (accessor value))
 
 
 {-| Create a new `Table`.
